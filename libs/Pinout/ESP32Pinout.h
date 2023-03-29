7977cd968cae37ddab35e7c95a56c91fc73894b1
Marcos Renan Krul <marcoskrul2k@gmail.com>
2023-03-29 18:13:37 -0300

    feat(libs): esp32 pinout

---
+++ libs/Pinout/ESP32Pinout.h
@@ -0,0 +1,32 @@
+#ifndef ESP32_PINOUT_H
+#define ESP32_PINOUT_H
+
+
+#define D15 15
+#define D2 2
+#define D4 3
+#define RX2 16
+#define TX2 17
+#define D5 5
+#define D18 18
+#define D19 19
+#define D21 21
+#define RXD 3
+#define TXD 1
+#define D22 22
+#define D23 23
+#define D13 13
+#define D12 12
+#define D14 14
+#define D27 27
+#define D26 26
+#define D25 25
+#define D33 33
+#define D32 32
+#define D35 35
+#define D34 34
+#define VN 39
+#define VP 36
+
+
+#endif
