723b3f40f7468ad4d72ac49a50dbe08a153b38c9
Marcos Renan Krul <marcoskrul2k@gmail.com>
2023-03-30 07:36:31 -0300

    refact(sketch): use wifi connection lib

--- src/Arduino_IDE/sketch_main/sketch_main.ino
+++ src/Arduino_IDE/sketch_main/sketch_main.ino
@@ -1,27 +1,25 @@
-/*
-  PLACA: ESP32-WROOM-DA Module
-*/
-#include <WiFi.h>
+#include <ESP32Pinout.h>
+#include <WiFiConnection.h>
 
-#define WIFI_SSID "StatusNet 20 303"
-#define WIFI_PASSWORD "98706993"
+#define PIN_LED_WIFI_FEEDBACK D21
 
+char wifi_ssid[32] = "";
+char wifi_password[32] = "";
 
-BluetoothSerial SerialBT;
+WiFiConnection wiFiConnection = WiFiConnection(wifi_ssid, wifi_password);
 
-void setup() {
- Serial.begin(115200);
- WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
- 
- while (WiFi.status() != WL_CONNECTED) {
-  delay(500);
-  Serial.println("Connecting to WiFi..");
- }
- Serial.println("Connected to the WiFi network");
+void setup()
+{
+  Serial.begin(115200);
 
+  pinMode(PIN_LED_WIFI_FEEDBACK, OUTPUT);
 }
 
-void loop() {
-  delay(2000);
-          
+void loop()
+{
+  if (!wiFiConnection.connected()) wiFiConnection.reconnect();
+
+  digitalWrite(PIN_LED_WIFI_FEEDBACK, wiFiConnection.connected());
+
+  delay(50);
 }
